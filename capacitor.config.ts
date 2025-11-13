dde62e3e06ae5e91e43c90adb2d06d07ac154546
rafie4real <rafie4real@gmail.com>
2025-11-13 13:37:41 +0000

    fix: capacitor android api issues

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,8 @@ const config: CapacitorConfig = {
   appName: 'Booksnap',
   webDir: 'out',
   server: {
+    url: 'http://192.168.1.100:3000',
+    cleartext: true,
     androidScheme: 'https',
     allowNavigation: [
       "covers.openlibrary.org",
